0d83a0e9cde1c761648d761d85a096d3e024c2f6
Marvit1 <arsenbg2014@gmail.com>
2024-12-04 15:56:34 +0400

    g

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,9 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
-
   plugins: [
     '~/plugins/axios.js', // Register your plugin here
   ],
-  
+
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
@@ -14,22 +13,20 @@ export default defineNuxtConfig({
     },
   },
   modules: ['@pinia/nuxt', '@stefanobartoletti/nuxt-social-share'],
-  
+
   runtimeConfig: {
     public: {
       apiBase: 'https://backo-aa1b430f6434.herokuapp.com/api/v1', // Always use Heroku
-      
     },
- 
-  
   },
+
   app: {
     head: {
       title: 'Data Politics',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: ' News' },
+        { name: 'description', content: 'News' },
         { name: 'keywords', content: 'news, updates, articles, latest news' },
         { name: 'robots', content: 'index, follow' },
         // Open Graph Meta Tags
@@ -42,13 +39,11 @@ export default defineNuxtConfig({
         { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'Data Politics' },
         { name: 'twitter:description', content: 'News' },
-        {name: "og:image", content: ""},
       ],
-     
-        link: [
-          { rel: 'icon', type: 'image/png',  href: '/fav.png' },
-        ],
-      
-    }
-  }
-})
+
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/fav.png' }, // Corrected to /fav.png
+      ],
+    },
+  },
+});
