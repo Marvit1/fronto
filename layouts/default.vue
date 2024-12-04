acd523e4a370c6a45a519f1cfb02fc4dc5a32c8e
Marvit1 <arsenbg2014@gmail.com>
2024-12-04 16:58:33 +0400

    icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <template>
     <div>
-      <head>
-        <link rel="icon" type="image.png" href="https://www.google.com/imgres?q=png%20data%20politics&imgurl=https%3A%2F%2Fassets.genially.com%2Fs3fs-public%2FPolitical%2520data%2520videoen.png%3FVersionId%3DRIbsNy.tn2VksT1OFvXHd.NbrU3DEx8w&imgrefurl=https%3A%2F%2Fgenially.com%2Ftemplate%2Fpolitical-data-video%2F&docid=wAVmprzPCBITfM&tbnid=DH4NYaBGk_ozoM&vet=12ahUKEwj227O8kY6KAxVQA9sEHeHjMOwQM3oECEcQAA..i&w=600&h=336&hcb=2&ved=2ahUKEwj227O8kY6KAxVQA9sEHeHjMOwQM3oECEcQAA" />
-      </head>
+     
       <Header />
       <NuxtLayout>
       <NuxtPage />
